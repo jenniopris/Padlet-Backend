4eba40396f76cdda3889a7dbc7a4fae9ab7b76c9
jenniopris <s2010456021@students.fh-hagenberg.at>
2023-05-24 17:43:59 +0200

    small changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,36 +4,36 @@
     <title>Padlet Backend</title>
 </head>
 <body>
-    <h1>This is my backend, wohoo</h1>
-    <h2>List of Padlets</h2>
+    <h1>This is the backend of my Padlets-Application</h1>
+    <h2>List of all Padlets</h2>
     <ol>
         @foreach($padlets as $padlet)
             <li>{{ $padlet->name }}</li>
         @endforeach
     </ol>
 
-    <h2>List of Users</h2>
+    <h2>List of all Users</h2>
     <ol>
         @foreach($users as $user)
             <li>{{ $user->first_name ." ". $user->last_name }}</li>
         @endforeach
     </ol>
 
-    <h2>List of Entries</h2>
+    <h2>List of all Entries</h2>
     <ol>
         @foreach($entries as $entry)
             <li>{{ $entry->name }}</li>
         @endforeach
     </ol>
 
-    <h2>List of Comments</h2>
+    <h2>List of all Comments</h2>
     <ol>
         @foreach($comments as $comment)
             <li>{{ $comment->comment }}</li>
         @endforeach
     </ol>
 
-    <h2>List of Ratings</h2>
+    <h2>List of all Ratings</h2>
     <ol>
         @foreach($ratings as $rating)
             <li>{{ $rating->rating }}</li>
